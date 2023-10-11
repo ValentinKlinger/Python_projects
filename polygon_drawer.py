36d75a18c8801f08d16e862f554ab3003d2a4769
valentin <valentink.fr@gmail.com>
2023-10-11 19:00:20 +0200

    Added ability to draw a second poligon. (polygon_drawer.py)

--- polygon_drawer.py
+++ polygon_drawer.py
@@ -7,10 +7,13 @@ class PolygonDrawer:
         self.root = root
         self.canvas = tk.Canvas(root, width=600, height=600, bg="ivory")
         self.canvas.pack()
-        self.points = []
-        self.drawing = True
+        self.points_pgn1 = []
+        self.points_pgn2 = []
+        self.drawing_pgn1 = True
+        self.drawing_pgn2 = False
 
-        self.canvas.create_polygon(0, 0, outline="black", width=3, fill="", tag="pgn")
+        self.canvas.create_polygon(0, 0, outline="blue", width=3, fill="", tag="pgn1")
+        self.canvas.create_polygon(0, 0, outline="red", width=3, fill="", tag="pgn2")
 
         self.canvas.bind("<Button-1>", self.add_point)
         self.canvas.bind("<Motion>", self.show_live_polygon)
@@ -20,41 +23,84 @@ class PolygonDrawer:
         Show the live polygone with current coordinates and mouse's position.
         """
 
-        x, y = event.x, event.y
-
-        if len(self.points) > 1 and self.drawing == True:
-            polygon_points = self.points + [(x, y)]
-            self.canvas.coords(
-                "pgn", [coord for point in polygon_points for coord in point]
-            )
+        if self.drawing_pgn1:
+            x, y = event.x, event.y
+            if len(self.points_pgn1) > 1:
+                polygon_points = self.points_pgn1 + [(x, y)]
+                self.canvas.coords(
+                    "pgn1", [coord for point in polygon_points for coord in point]
+                )
+        elif self.drawing_pgn2:
+            x, y = event.x, event.y
+            if len(self.points_pgn2) > 1:
+                polygon_points = self.points_pgn2 + [(x, y)]
+                self.canvas.coords(
+                    "pgn2", [coord for point in polygon_points for coord in point]
+                )
 
     def add_point(self, event):
         """
         Add a new point to the list of points when user left click.
         """
-        if self.drawing:
+        if self.drawing_pgn1:
             x, y = event.x, event.y
 
             if (
-                len(self.points) != 0
-                and sqrt((x - self.points[0][0]) ** 2 + (y - self.points[0][1]) ** 2)
+                len(self.points_pgn1) != 0
+                and sqrt(
+                    (x - self.points_pgn1[0][0]) ** 2
+                    + (y - self.points_pgn1[0][1]) ** 2
+                )
                 < 20
             ):  # if the user has finished drawing the polygon
-                self.drawing = False
+                self.drawing_pgn1 = False
+                self.drawing_pgn2 = True
                 yellow_circle = self.canvas.find_withtag("yellow_circle")
                 self.canvas.delete(yellow_circle)
                 self.canvas.coords(
-                    "pgn", [coord for point in self.points for coord in point]
+                    "pgn1", [coord for point in self.points_pgn1 for coord in point]
                 )
 
             else:
-                self.points.append((x, y))
-                if len(self.points) == 1:  # If it's the first point.
+                self.points_pgn1.append((x, y))
+                if len(self.points_pgn1) == 1:  # If it's the first point.
                     self.canvas.create_oval(
-                        self.points[0][0] - 20,
-                        self.points[0][1] - 20,
-                        self.points[0][0] + 20,
-                        self.points[0][1] + 20,
+                        self.points_pgn1[0][0] - 20,
+                        self.points_pgn1[0][1] - 20,
+                        self.points_pgn1[0][0] + 20,
+                        self.points_pgn1[0][1] + 20,
+                        fill="#F6FF66",
+                        width=0,
+                        tag="yellow_circle",
+                    )
+                    self.canvas.tag_lower("yellow_circle")
+
+        elif self.drawing_pgn2:
+            x, y = event.x, event.y
+
+            if (
+                len(self.points_pgn2) != 0
+                and sqrt(
+                    (x - self.points_pgn2[0][0]) ** 2
+                    + (y - self.points_pgn2[0][1]) ** 2
+                )
+                < 20
+            ):  # if the user has finished drawing the polygon
+                self.drawing_pgn2 = False
+                yellow_circle = self.canvas.find_withtag("yellow_circle")
+                self.canvas.delete(yellow_circle)
+                self.canvas.coords(
+                    "pgn2", [coord for point in self.points_pgn2 for coord in point]
+                )
+
+            else:
+                self.points_pgn2.append((x, y))
+                if len(self.points_pgn2) == 1:  # If it's the first point.
+                    self.canvas.create_oval(
+                        self.points_pgn2[0][0] - 20,
+                        self.points_pgn2[0][1] - 20,
+                        self.points_pgn2[0][0] + 20,
+                        self.points_pgn2[0][1] + 20,
                         fill="#F6FF66",
                         width=0,
                         tag="yellow_circle",
